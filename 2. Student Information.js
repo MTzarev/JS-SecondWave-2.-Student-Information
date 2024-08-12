f6463e9a413baefdc62baf8566025dc9cf467f0a
Miroslav Tsarev <mtzarev@gmail.com>
2024-08-12 23:38:56 +0300

    Add files via upload

---
+++ 2. Student Information.js
@@ -0,0 +1,9 @@
+function studentInformation(name, age, avgGrade){
+// let name = solve[0];
+// let age = solve [1];
+// let avgGrade = solve[2];
+
+console.log(`Name: ${name}, Age: ${age}, Grade: ${avgGrade.toFixed(2)}`);
+
+}
+studentInformation('John', 15, 5.54678)
